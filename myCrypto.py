81c872008488e617da869ff154456d2ecfad88a0
Robbie Rayas <robbierayas@gmail.com>
2017-05-10 01:01:53 +0000

    got most of it working butvalue wrong

--- myCrypto.py
+++ myCrypto.py
@@ -81,66 +81,71 @@ rho={
 	0:7,1:4,2:13,3:1,4:10,5:6,6:15,7:3,8:12,9:0,10:9,11:5,12:2,13:14,14:11,15:8
 }
 
+#overflow mask 2^32
+mask=4294967296;
 
-def myRipeMD160(test_input):
-	rinput=hashlib.sha256(test_input.decode('hex')).digest()
+def myRipeMD160(public_key):
+	print 'Begin myRipeMD160, public key input'	
+        message=hashlib.sha256(public_key.decode('hex')).hexdigest()
+        print 'message {}'.format(message)
+	bmessage=bin(int(message,16))[2:]
+	print 'bmessage {}'.format(bmessage)
+	print 'bmessage type {}'.format(type(bmessage))
+	length=len(bmessage)
+	print 'bmessage bit length {}'.format(length)
 
-	length=int(len(rinput)*math.log(16,2))
-	print 'bit size {}'.format(length)
-	ripemd160 = hashlib.new('ripemd160')
-	ripemd160.update(rinput)
-	ripe=utils.base58CheckEncode(0, ripemd160.digest())
-
-	
-        input=hashlib.sha256(test_input.decode('hex')).hexdigest()
-
+	print ''
+	print 'message divided into {} blocks'.format(int(1+(length+64)/512))	
 
 	#pad messages so its length is 448 mod 512
 	numberOfZeros=int(511-((length+64)%512))
-	print 'zeros {}'.format(numberOfZeros)
-        print 'input {}'.format(input)
-	#print "hex? {}".format(int(input,16))
-	binput=bin(int(input,16))[2:]
-	print binput
-	print type(binput)
+	print 'zeros needed {}'.format(numberOfZeros)
 
         #append a 64 bit length value to message
         blength=bin(length)[2:].zfill(64)
-	binput=binput.ljust(length+1,'1').ljust(448,'0')+blength
-	print 'bit size {}'.format(len(binput))
-	print 'message length {}'.format(binput)
-	hinput=hex(int(binput,2))[2:-1]
-        print 'message length {}'.format(hinput)
-
-	#binput_check=bin(int(hinput,16))[2:]
-        #print 'binput_check {}'.format(binput_check)
-        #print type(binput_check)
-
+	print 'bmessage bit blength {}'.format(blength)
+#LSB FIRST???
+	print ''
 	
-	
+	#append zeros and length
+	bmessage=bmessage.ljust(length+1,'1').ljust(448,'0')+blength
+	print 'new bmessage {}'.format(bmessage)
+	print 'new bmessage bit length {}'.format(len(bmessage))
 
-	#split message to 16 32-bit blocks for X[]
-	X=[hinput[i:i+8] for i in range(0, len(hinput)-1, 8)]
-	print 'split message {}'.format(X)
 
+	print ''
+	hmessage=hex(int(bmessage,2))[2:-1]
+        print 'hmessage {}'.format(hmessage)
 
+	#bmessage_check=bin(int(hmessage,16))[2:]
+        #print 'bmessage_check {}'.format(bmessage_check)
+        #print type(bmessage_check)
+
+	print ''
+	print ''	
+
+	#split message to 16 32-bit words for X[]
+	X=[hmessage[i:i+8] for i in range(0, len(hmessage)-1, 8)]
+	print 'split hmessage into 16 words X {}'.format(X)
+
+	print 'for each 512-block t in the input message, run loop t-1 times'
 	#initialise 5 word 160 bit buffer (ABCDE) to ()
 	Ai=int('67452301',16)
 	Bi=int('efcdab89',16)
 	Ci=int('98badcfe',16)
 	Di=int('10325476',16)
 	Ei=int('c3d2e1f0',16)
-        A=Ai
+	A=Ai
         B=Bi
-        C=Ci
+	C=Ci
         D=Di
-        E=Ei
+	E=Ei
 	Ar=A
 	Br=B
 	Cr=C
 	Dr=D
 	Er=E
-
+	print 'Xold {} {} {} {} {}'.format(A,B,C,D,E)
 	#process message in 16_word (512 bit) chunks
 	##use 10 rounds of 16 bit ops on message block and buffer - in 2 parallel lines of 5
 	for round in range(0,5):
@@ -154,10 +159,9 @@ def myRipeMD160(test_input):
 				r=j
 			rr=(pi_j*rho_j^round)%16
 
-#??????????????#
 			Xl=int(X[r],16)
 			Xr=int(X[rr],16)
-#??????????????#
+
 			#K
 			K=int(constantmap_left.get(round, "nothing"),16)
 	                Kr=int(constantmap_right.get(round, "nothing"),16)
@@ -166,141 +170,87 @@ def myRipeMD160(test_input):
 			s=shiftmap_left.get(round*15+j,"nothing")
                         sr=shiftmap_right.get(round*15+j,"nothing")
 
-
 			#left
 			A,B,C,D,E=compression(A,B,C,D,E,functionmap_left.get(round, "nothing"),Xl,K,s)
 
-                       #right
+                        #right
                         Ar,Br,Cr,Dr,Er=compression(Ar,Br,Cr,Dr,Er,functionmap_right.get(round, "nothing"),Xr,K,s)
-#		next?
 
-
-	##add output to input to form new buffer value
+	print 'loop finished'
+	##add output to message to form new buffer value
 	#convert h0, h1, h2, h3 and h4 in hex, then add, low order first???
-	Af=Bi+C+Dr
-	Bf=Ci+D+Er
-	Cf=Di+E+Ar
-	Df=Ei+A+Br
-	Ef=Ai+B+Cr
-	print 'Af {}'.format(Af)
+	Af=(Bi+C+Dr)%mask
+	Bf=(Ci+D+Er)%mask
+	Cf=(Di+E+Ar)%mask
+	Df=(Ei+A+Br)%mask
+	Ef=(Ai+B+Cr)%mask
+	Af_hex=hex(Af)
+	Bf_hex=hex(Bf)
+	Cf_hex=hex(Cf)
+	Df_hex=hex(Df)
+	Ef_hex=hex(Ef)
 
-	answer=0
-	hex_data1 = Af+Bf+Cf+Df+Ef;
-	answer=answer+hex_data1
-	print 'ripe1 {}'.format(ripe)
+	hex_data1 =(Af<<128)+(Bf<<96)+(Cf<<64)+(Df<<32)+Ef
+	answer=hex(hex_data1)[2:-1]
+
 	print 'ripe2 {}'.format(answer)
 	#output hash value is the final buffer value
 	ripe2=answer
-        hashLength=len(ripe2)*math.log(16,2)
-        print 'bit size {}'.format(hashLength)
-
-
-
-
-
-	return ripe
+	return ripe2
 
 def compression(A,B,C,D,E,f,X,K,s):
+	#print 'compression {}'.format(A)
 	A_out,C_out=operation(A,B,C,D,E,f,X,K,s)
-        A=E
+        A=E%mask
+        C=B%mask
+        E=D%mask
         B=A_out
-        C=B
         D=C_out
-        E=D
 	return A,B,C,D,E
 
 def operation(A,B,C,D,E,f,X,K,s):
 	A_out=A
 	if f==1:
-		A_out=A_out+function1(B,C,D)
+		A_out=(A_out+function1(B,C,D))%mask
 	elif f==2:
-                A_out=A_out+function2(B,C,D)
+                A_out=(A_out+function2(B,C,D))%mask
         elif f==3:
-                A_out=A_out+function3(B,C,D)
+                A_out=(A_out+function3(B,C,D))%mask
         elif f==4:
-                A_out=A_out+function4(B,C,D)
+                A_out=(A_out+function4(B,C,D))%mask
         elif f==5:
-                A_out=A_out+function5(B,C,D)
-	A_out=A_out+X
-	A_out=A_out+K
-	A_out=cyclicShift(A_out,s)
-	A_out=A_out+E
-	C_out=cyclicShift(C,10)
-	return A_out,C_out
+                A_out=(A_out+function5(B,C,D))%mask
+	A_out=(A_out+X)%mask
+	A_out=(A_out+K)%mask
+	A_out=cyclicShift(A_out,s)%mask
+	A_out=(A_out+E)%mask
+	C_out=cyclicShift(C,10)%mask
+	return A_out%mask,C_out%mask
 
 def ordering(i):
 	return ordermap.get(i, "nothing")
 
 def function1(B,C,D):
-#	print 'function 1'
-	binB=int(bin(B)[2:],2)
-	
-        binC=int(bin(C)[2:],2)
-
-        binD=int(bin(D)[2:],2)
-#	print 'binB {}'.format(binB)
-#        print 'binC {}'.format(binC)
-#        print 'binD {}'.format(binD)
-
-        num=binB^binC^binD
-	#print 'B {}'.format(int(bin(B)))
-        #print 'C {}'.format(bin(C))
-        #print 'D {}'.format(bin(D))
-	#num=bin(bin(B)^bin(C)^bin(D))
-	print num
+        num=B^C^D
 	return num
 
 def function2(B,C,D):
-
-#        print 'function 2'
-        binB=int(bin(B)[2:],2)
-
-        binC=int(bin(C)[2:],2)
-
-        binD=int(bin(D)[2:],2)
-        num=(binB&binC)|(~binB&binD)
-
-#        num=bin((bin(B)&bin(C))|(~bin(B)&bin(D)))
+        num=(B&C)|(~B&D)
         return num
 
 def function3(B,C,D):
-#        print 'function 3'
-        binB=int(bin(B)[2:],2)
-
-        binC=int(bin(C)[2:],2)
-
-        binD=int(bin(D)[2:],2)
-        num=(binB|~binC)^binD
-
-#        num=bin((bin(B)|~bin(C))^bin(D))
+        num=(B|~C)^D
         return num
-
 
 def function4(B,C,D):
-#        print 'function 4'
-        binB=int(bin(B)[2:],2)
-
-        binC=int(bin(C)[2:],2)
-
-        binD=int(bin(D)[2:],2)
-        num=(binB&binD)|(binC&~binD)
-
-#        num=bin((bin(B)&bin(D))|(bin(C)&~bin(D)))
+        num=(B&D)|(C&~D)
         return num
 
-
 def function5(B,C,D):
-#        print 'function 5'
-        binB=int(bin(B)[2:],2)
-
-        binC=int(bin(C)[2:],2)
-
-        binD=int(bin(D)[2:],2)
-	num=binB^binC|~binD
-
-#       num=bin(bin(B)^(bin(C)|~bin(D)))
+	num=B^(C|~D)
         return num
 
 def cyclicShift(C,s):
-	C_rot=C
+	C_rot=C<<s
+	C_rot=C_rot%mask+C_rot/mask
 	return C_rot
--- myTransaction.py
+++ myTransaction.py
@@ -9,34 +9,45 @@ import math
 import hashlib
 
 #public_key=myWallet.createAddress('4eb2eeac47c1dbba95a97cf32fd25a22410195bb64b64ba5f0fa6e0896ba31ca')
+print ''
+print 'MyTransaction: private key a2d43efac7e99b7e3cf4c07ebfebb3c349d8f2b5b0e1062d9cef93c170d22d4f'
+print ''
+print ''
+print ''
+print 'Method 1'
 public_key=myWallet.createAddress('a2d43efac7e99b7e3cf4c07ebfebb3c349d8f2b5b0e1062d9cef93c170d22d4f')
-print 'MyTransaction public_key {}'.format(public_key)
+print 'MyTransaction: public_key calulated using Mywallet lib {}'.format(public_key)
 
 pkHash=keyUtils.pubKeyToAddr(public_key)
-print 'MyTransaction pkHash {}'.format(pkHash)
-
-#pkHashStep1=hashlib.sha256(public_key.decode('hex')).digest()
+print 'MyTransaction: pkHash calulated using KeyUtils lib {}'.format(pkHash)
+print ''
+print ''
+print ''
+print 'Method 2'
 pkHashStep1=hashlib.sha256(public_key.decode('hex')).digest()
-print 'MyTransaction pkHashStep1 {}'.format(pkHashStep1)
-print 'pkHashStep1 {}'.format(type(pkHashStep1))
+print 'MyTransaction: pkHashStep1 calculated from sha256(publickey) {}'.format(pkHashStep1)
+print 'MyTransaction: pkHashStep1 calculated from sha256(publickey) {}'.format(pkHashStep1.encode('hex'))
+print 'pkHashStep1 type {}'.format(type(pkHashStep1))
 
 ripemd160=hashlib.new('ripemd160')
 ripemd160.update(pkHashStep1)
-pkHashStep2=utils.base58CheckEncode(0, ripemd160.digest()) 
-print 'MyTransaction pkHashStep2 {}'.format(pkHashStep2)
+ripemd160digest=ripemd160.digest()
+print 'ripemd160digest {}'.format(ripemd160digest)
+print 'ripemd160digest {}'.format(ripemd160digest.encode('hex'))
+pkHashStep2=utils.base58CheckEncode(0, ripemd160digest) 
+print 'MyTransaction pkHashStep2 calculated from ripemd160 hashlib base58(RIPEMD160(SHA256(PK))) {}'.format(pkHashStep2)
+
+print ''
+print ''
+print ''
+print 'Method 3'
+pkHashStep21=myCrypto.myRipeMD160(public_key)
+print 'MyTransaction pkHashStep2 {}'.format(pkHashStep21)
+print 'MyTransaction pkHashStep2 {}'.format(pkHashStep21.decode('hex'))
+pkHashStep22=utils.base58CheckEncode(0, pkHashStep21) 
 
 
 
-
-pkHashStep21=hashlib.sha256(public_key.decode('hex')).digest()
-print 'MyTransaction pkHashStep1 {}'.format(pkHashStep21)
-print len(pkHashStep21)
-print 'pkHashStep1 {}'.format(type(pkHashStep21))
-
-
-#pkHashStep22=myCrypto.myRipeMD160(pkHashStep21)
-pkHashStep22=myCrypto.myRipeMD160(public_key)
-
-print 'MyTransaction pkHashStep2 {}'.format(pkHashStep22)
+print 'MyTransaction pkHashStep2 calculated from ripemd160 func base58(RIPEMD160(SHA256(PK))) {}'.format(pkHashStep22)
 
 
